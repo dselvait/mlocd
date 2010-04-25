d93583273d8c16ca76972daaa39ca23d664a7610
wang321gordon <wang321gordon@df46f746-27b3-2f79-71cb-7103b569e8fc>
2010-04-25 11:12:34 +0000

    update

---
+++ trunk/mlocd/src/test/TestAttribueSelection.java
@@ -0,0 +1,15 @@
+package test;
+import data.preprocessor.AttributeSelection;
+
+public class TestAttribueSelection {
+
+	public static void main(String[] args)throws Exception{
+		String base = ".\\";
+		String dataset = "cmc.data";
+		AttributeSelection as = new AttributeSelection();
+		int[] atts = as.recommandAttributes(base+dataset);
+	    for(int s:atts)
+	    	System.out.println(s);
+	    System.out.println("System End");
+	}
+}
