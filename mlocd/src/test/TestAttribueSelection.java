583151ca8ef880edd2bd0f49cf702883d91b74c8
wang321gordon <wang321gordon@df46f746-27b3-2f79-71cb-7103b569e8fc>
2010-04-26 09:46:21 +0000

    update

--- mlocd/src/test/TestAttribueSelection.java
+++ mlocd/src/test/TestAttribueSelection.java
@@ -5,7 +5,7 @@ public class TestAttribueSelection {
 
 	public static void main(String[] args)throws Exception{
 		String base = ".\\";
-		String dataset = "cmc.data";
+		String dataset = "german.data";
 		AttributeSelection as = new AttributeSelection();
 		int[] atts = as.recommandAttributes(base+dataset);
 	    for(int s:atts)
