3b82d882140f64d08645036d027be883e8dc6ca3
wang321gordon <wang321gordon@df46f746-27b3-2f79-71cb-7103b569e8fc>
2010-04-26 09:33:34 +0000

    add

---
+++ mlocd/src/utils/FileWorker.java
@@ -0,0 +1,46 @@
+package utils;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class FileWorker {
+  
+  public static void cp(String file1, String file2) throws Exception {
+	BufferedReader in = new BufferedReader(new FileReader(file1));
+	PrintWriter out=new PrintWriter(new FileOutputStream(file2)); 
+	String line;
+	while((line=in.readLine())!=null){
+	out.println(line);  
+	 }
+	out.close();
+	in.close();
+  }
+		
+  public static void remove(String filename) {
+	 File target = new File(filename);
+	 // Make sure the file or directory exists and isn't write protected
+	 if (!target.exists())
+	    throw new IllegalArgumentException(
+	          "Delete: no such file or directory: " + filename);
+
+	 if (!target.canWrite())
+	      throw new IllegalArgumentException("Delete: write protected: "
+	          + filename);
+
+	    // If it is a directory, make sure it is empty
+	 if (target.isDirectory()) {
+	    String[] files =target.list();
+	    if (files.length > 0)
+	       throw new IllegalArgumentException(
+	            "Delete: directory not empty: " + filename);
+	 }
+
+	 boolean success = target.delete();
+
+	 if (!success)
+	    throw new IllegalArgumentException("Delete: deletion failed");
+  }
+  
+}
