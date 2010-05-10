321bda72ff6f9e29946b41ff777072bd85b96b7d
wang321gordon <wang321gordon@df46f746-27b3-2f79-71cb-7103b569e8fc>
2010-05-10 16:32:31 +0000

    update

--- trunk/mlocd/src/run/RunMSC.java
+++ trunk/mlocd/src/run/RunMSC.java
@@ -4,6 +4,7 @@ import classifiers.NaiveWeka;
 import utils.Evaluation;
 
 import data.generator.Sea;
+import data.generator.Testset;
 
 public class RunMSC {
 	
@@ -11,20 +12,21 @@ public class RunMSC {
 		String base = System.getProperty("user.dir")+"\\lab\\";
 		String stadata = "sea";
 		Sea sea = new Sea(base+stadata);
-		String[] datasets = new String[2];
+		String[] datasets = new String[1];
 		
 		for(int i = 0; i < datasets.length; ++i){
 			String dataset = base + stadata + (i+1) +".data";
 			sea.setLocation(dataset);
-//			sea.generateData(100);
-//			sea.makeNamesFile();
+			sea.generateData(500);
+			sea.makeNamesFile();
 			datasets[i] = dataset;
 		}
 		
-		String testset = datasets[datasets.length-1];
+		String testset = base+"seatest.data";
 	
 		NaiveWeka learner = new NaiveWeka();
 		learner.build(datasets[0]);
+		Testset.make(datasets, testset, 200);
 		String[] labels = learner.classifyData(testset);
 		double accuracy = Evaluation.accuracy(testset, labels);
 		System.out.println("The accuracy of msc is "+accuracy);
